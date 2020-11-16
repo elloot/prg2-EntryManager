b19a4a09c1c5d68346bdf4ddf530a64e934fd7af
elloot <elliot.duchek@gmail.com>
2020-11-16 12:07:14 +0100

    remove unnecessary fileIO variable

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
         EntryManager entryManager = new EntryManager();
         View view = new View("EntryManager");
-        FileIO fileIO = new FileIO("EntryManager.obj");
-        Controller controller = new Controller(entryManager, view, fileIO);
-
-        controller.writeEntryManagerToFile();
+        Controller controller = new Controller(entryManager, view, new FileIO());
     }
 }
